53a4cd659104244041d9d49d4c3464aab9ce52a6
Mohammed Jabir <med.jabir@gmail.com>
2020-03-03 01:08:18 +0100

    Regular commit

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function callSendAPI(sender_psid, response, game_page_access_token) {
 	//Send the HTTP request to the Messenger Platform
 	request(
 		{
-			uri: "https://graph.facebook.com/v2.6/me/messages",
+			uri: "https://graph.facebook.com/me/messages",
 			qs: { access_token: game_page_access_token },
 			method: "POST",
 			json: true,
